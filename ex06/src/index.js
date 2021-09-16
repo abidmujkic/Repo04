d2b952ed87710683d7b067dbc4368966e8d83d0d
abidmujkic <abidmujkic1@gmail.com>
2021-09-16 23:10:00 +0200

    ex06

---
+++ ex06/src/index.js
@@ -0,0 +1,17 @@
+function main(numOne, numTwo) {
+    // Only change code below this line
+    
+    var myQuotient;
+    myQuotient = numOne / numTwo;
+   
+  
+    // Only Change code above this line
+    
+    return myQuotient;
+       
+}
+
+
+console.log(main(5, 2)); // change this line
+
+module.exports = main;
