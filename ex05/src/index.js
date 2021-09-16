1cda22275f3e4d11bfc9870abd8732cb8371e4c6
abidmujkic <abidmujkic1@gmail.com>
2021-09-16 22:40:16 +0200

    ex05

---
+++ ex05/src/index.js
@@ -0,0 +1,17 @@
+function main(numOne, numTwo) {
+    // Only change code below this line
+    
+    var product;
+   product = numOne * numTwo;
+   
+  
+    // Only Change code above this line
+    
+    return product;
+       
+}
+
+
+console.log(main(5.5, 5.5)); // change this line
+
+module.exports = main;
