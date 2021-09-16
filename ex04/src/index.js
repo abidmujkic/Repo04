dc81f9d1612ab5b7a206d01165a5391fc203edd9
abidmujkic <abidmujkic1@gmail.com>
2021-09-16 15:58:04 +0200

    ex04

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,9 +1,10 @@
 function main() {
-    var myDecimal = 8.2;
-    var myDecimalE = 254e-5;
+    
     // Only change code below this line
     
-    
+    var myDecimal = 8.2;
+    var myDecimalE = 254e-5;
+
     // Only Change code above this line
     return {
         myDecimal,
