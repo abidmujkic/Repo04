bfa1e5f264af9908d4cc89c5e457ce270cd5e1c3
abidmujkic <abidmujkic1@gmail.com>
2021-09-16 14:38:51 +0200

    ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+    var myRes = 75;
+    // Only change code below this line
+    
+    // Only Change code above this line
+    myRes = ++myRes;
+    return myRes;
+}
+console.log(main());
+module.exports = main;
