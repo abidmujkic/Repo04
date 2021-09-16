1f47b7e8f855a5b23f3ab3b166e92cc1d30623dd
abidmujkic <abidmujkic1@gmail.com>
2021-09-16 23:55:38 +0200

    ex08

---
+++ ex08/src/index.js
@@ -0,0 +1,27 @@
+function main() {
+    // Only change code below this line
+
+    var myX = 12;
+    var myY = 19;
+    var myZ = 24;
+
+    myX += 3;
+    myY += 17;
+    myZ -= 20;
+   
+  
+    // Only Change code above this line
+    
+    return {
+        myX,
+        myY,
+        myZ,
+
+    };
+       
+}
+
+
+console.log(main()); // change this line
+
+module.exports = main;
