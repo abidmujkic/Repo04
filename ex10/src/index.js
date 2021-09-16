1ed22cd08700b863f1ba63715415235613c62363
abidmujkic <abidmujkic1@gmail.com>
2021-09-17 00:01:30 +0200

    ex10

---
+++ ex10/src/index.js
@@ -0,0 +1,24 @@
+function main() {
+    // Only change code below this line
+
+    var myFirstName = "Abid";
+    var myLastName = "Mujkic";
+    var myHomeTown = "Srebrenik";
+
+   
+  
+    // Only Change code above this line
+    
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown,
+
+    };
+       
+}
+
+
+console.log(main()); // change this line
+
+module.exports = main;
